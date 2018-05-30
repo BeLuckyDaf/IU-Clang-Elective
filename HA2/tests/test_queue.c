dec4ff700c92a171ca202d22a2bd5bd277044b2c
BoogiemanNSK <calah47@yandex.ru>
2018-05-30 21:35:11 +0300

    Added tests

---
+++ HA2/tests/test_queue.c
@@ -0,0 +1,40 @@
+#include <check.h>
+#include "../headers/queue.h"
+
+START_TEST (queue_test)
+{
+    QUEUE* q = create_queue();
+
+    queue_enqueue(q, 1);
+    queue_enqueue(q, 2);
+    queue_enqueue(q, 3);
+
+    ck_assert(queue_dequeue(q) == 1);
+    ck_assert(queue_peek(q) == 2);
+
+    delete_queue(q);
+}
+END_TEST
+
+Suite* str_suite (void)
+{
+    Suite *suite = suite_create("Home Assignment 2: Queue");
+    TCase *tcase = tcase_create("case");
+
+    tcase_add_test(tcase, queue_test);
+
+    suite_add_tcase(suite, tcase);
+    return suite;
+}
+
+int main (int argc, char *argv[]) {
+    int number_failed;
+    Suite *suite = str_suite();
+
+    SRunner *runner = srunner_create(suite);
+    srunner_run_all(runner, CK_NORMAL);
+
+    number_failed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+    return number_failed;
+}
